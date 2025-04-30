3b60f7f45a504ac89fbdffc1e957fac900e492d3
Gaurav Gupta <gaurav.gupta.jc@gmail.com>
2025-04-30 08:03:53 +0530

    Release version 2.8

--- src/main/java/io/github/jeddict/ai/JeddictUpdateManager.java
+++ src/main/java/io/github/jeddict/ai/JeddictUpdateManager.java
@@ -43,7 +43,7 @@ public class JeddictUpdateManager {
     }
 
     private static String getCurrentJeddictVersion() {
-        return "2.7";
+        return "2.8";
     }
 
     private File saveFile;
