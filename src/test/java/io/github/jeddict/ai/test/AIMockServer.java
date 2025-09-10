1ae84474af3c7e20950537df59e00a771a7a9687
Stefano Fornari <stefano.fornari@gmail.com>
2025-09-10 14:45:09 +0200

    Dumping the body so it can be inspected

--- src/test/java/io/github/jeddict/ai/test/AIMockServer.java
+++ src/test/java/io/github/jeddict/ai/test/AIMockServer.java
@@ -1,8 +1,26 @@
+/**
+ * Copyright 2025 the original author or authors from the Jeddict project (https://jeddict.github.io/).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package io.github.jeddict.ai.test;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import java.io.File;
+import java.io.FileWriter;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -72,6 +90,14 @@ public class AIMockServer {
             String error = "";
 
             String body = new String(exchange.getRequestBody().readAllBytes(), StandardCharsets.UTF_8);
+
+            //
+            // dump the body for inspection
+            //
+            File bodyFile = File.createTempFile("mockserver-", "-body");
+            try (final FileWriter w = new FileWriter(bodyFile)) {
+                w.append(body);
+            }
 
             Matcher matcher = MOCK_INSTRUCTION_PATTERN.matcher(body);
 
