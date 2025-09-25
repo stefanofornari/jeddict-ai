a5a1a4cf7618d1f5b63583f0f18f5d113f74f072
Stefano Fornari <stefano.fornari@gmail.com>
2025-09-25 08:29:37 +0200

    the use of propertyChangeLIstener seems to kick a dependency on AWT and therefore on having a display disabling until fixed

--- src/test/java/io/github/jeddict/ai/lang/JeddictBrainTest.java
+++ src/test/java/io/github/jeddict/ai/lang/JeddictBrainTest.java
@@ -77,6 +77,8 @@ public class JeddictBrainTest {
     }
 
     @Test
+    @Disabled // this seems to kick a dependency on AWT and therefore having a display;
+              // disabling until fixed
     public void add_and_remove_listeners() {
         final DummyStreamHandler H = new DummyStreamHandler();
         final String N = "jeddict";
