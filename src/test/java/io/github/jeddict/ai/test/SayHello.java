be1ecc87d821ece21348359ae462d323395588d5
Stefano Fornari <stefano.fornari@gmail.com>
2025-10-03 17:52:57 +0200

    reverted changes committed by mistake

--- src/test/java/io/github/jeddict/ai/test/SayHello.java
+++ src/test/java/io/github/jeddict/ai/test/SayHello.java
@@ -1,10 +1,6 @@
 package io.github.jeddict.ai.test;
 
 public class SayHello {
-    /**
-     * Prints a hello message to standard output.
-     * @param name the name to include in the hello message
-     */
     public void sayHello(String name) {
         System.out.println("Hello, " + name + "!");
     }
