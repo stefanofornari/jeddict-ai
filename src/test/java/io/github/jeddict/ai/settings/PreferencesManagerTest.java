c192eb4467e5dbf496ca72a290124e613433be48
Stefano Fornari <ste@macbook-2.homenet.telecomitalia.it>
2025-10-13 18:10:34 +0200

    initial revision

---
+++ src/test/java/io/github/jeddict/ai/settings/PreferencesManagerTest.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright 2025 the original author or authors from the Jeddict project (https://jeddict.github.io/).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.github.jeddict.ai.settings;
+
+import io.github.jeddict.ai.test.TestBase;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ */
+public class PreferencesManagerTest extends TestBase {
+    
+    @Test
+    public void constructor_without_given_path() {
+        //
+        // If no path is provided, the system settings dir is picked (for macos
+        // windows or XDG linux
+        //
+        
+        //
+        // Pretend to be linux
+        //
+        
+    }
+    
+}
