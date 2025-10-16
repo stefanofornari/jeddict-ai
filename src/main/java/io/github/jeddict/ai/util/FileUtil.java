fd22a0b67d1bbab3bc45173546ed866ff81233dd
Stefano Fornari <stefano.fornari@gmail.com>
2025-10-16 07:26:05 +0200

    trying to fix path issue on windows

--- src/main/java/io/github/jeddict/ai/util/FileUtil.java
+++ src/main/java/io/github/jeddict/ai/util/FileUtil.java
@@ -110,6 +110,9 @@ public class FileUtil {
         final String os = System.getProperty("os.name").toLowerCase();
         final Path userHome = Paths.get(System.getProperty("user.home"));
 
+        System.out.println(">> os: " + os);
+        System.out.println(">> home: " + userHome);
+
         if (os.contains("win")) {
             final String appData = System.getenv("APPDATA");
             final Path basePath;
