eaef5502b83a23ddbe4f3fae07f9ff0d2e25dece
Stefano Fornari <stefano.fornari@gmail.com>
2025-10-17 08:34:29 +0200

    trying to fix path issue on windows

--- src/test/java/io/github/jeddict/ai/settings/PreferencesManagerTest.java
+++ src/test/java/io/github/jeddict/ai/settings/PreferencesManagerTest.java
@@ -167,7 +167,6 @@ public class PreferencesManagerTest extends TestBase {
     }
 
     @Test
-    @Disabled // TODO: make it work in the CI/CD pipeline
     public void migrates_old_config_file_from_home_directory_windows_appdata() throws Exception {
         final Path USERHOME = HOME.resolve(USER);
         SystemLambda.restoreSystemProperties(() -> {
