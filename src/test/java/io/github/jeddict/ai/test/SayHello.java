5271e63aeaf8400a7a2eb9ccad40c8a4455c56fd
Stefano Fornari <stefano.fornari@gmail.com>
2025-10-26 08:58:51 +0100

    Updated to support code suggestion testing

--- src/test/java/io/github/jeddict/ai/test/SayHello.java
+++ src/test/java/io/github/jeddict/ai/test/SayHello.java
@@ -1,3 +1,7 @@
+//
+// DO NOT CHANGE THIS CLASS. IT IS USED IN MANY TEST CASES AND POSITION OF TEXT
+// IS RELEVANT AND IMPORTANT.
+//
 package io.github.jeddict.ai.test;
 
 import jakarta.ws.rs.Path;
@@ -5,6 +9,12 @@ import jakarta.ws.rs.Path;
 @Path("/greetings")  // to test create rest endpoint hint
 public class SayHello {
     public void sayHello(String name) {
-        System.out.println("Hello, " + name + "!");
+        if (name != null) {
+            System.out.println("Hello, " + name + "!");
+        } else {
+            System.out.println("What's your name?");
+        }
     }
+
+    class InnerClass { } // to test inner class suggestions
 }
